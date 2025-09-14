f3ce1c6bcb51b3e225f9e9e6c38e028993fde427
Md. Ratan Chowdhury <ratanchowdhury360>
2025-09-15 01:41:52 +0600

    Delete menu item with VerifyAdmin middleware

--- index.js
+++ index.js
@@ -136,18 +136,29 @@ async function run() {
 
         // menu related apis
 
+        // menu related apis
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
-        }
-        );
+        });
 
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
-      const item = req.body;
-      const result = await menuCollection.insertOne(item);
-      res.send(result);
-        }
-        );
+            const item = req.body;
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        });
+
+        app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
 
         //cart collection
         app.get('/carts', async (req, res) => {
